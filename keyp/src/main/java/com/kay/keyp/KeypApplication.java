04f52684741a4fca07ff3215d8133e5afc0044f3
Krishnaraj <krishnaraj.vmet@gmail.com>
2025-08-26 11:29:45 +0530

    Initial Project Commit

---
+++ keyp/src/main/java/com/kay/keyp/KeypApplication.java
@@ -0,0 +1,13 @@
+package com.kay.keyp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KeypApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KeypApplication.class, args);
+	}
+
+}
---
+++ keyp/src/test/java/com/kay/keyp/KeypApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kay.keyp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KeypApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
